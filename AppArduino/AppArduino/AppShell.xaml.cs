1bcc9310e6796c9cfedf94844c343930c10b0b2a
patrycjalewandowska <24plewandowska@gmail.com>
2022-07-26 13:41:52 +0200

    fix first window

---
+++ AppArduino/AppArduino/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace AppArduino;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ AppArduino/AppArduino/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace AppArduino;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
