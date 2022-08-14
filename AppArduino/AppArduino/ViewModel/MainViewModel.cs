845bcbb0ab885bd4a33b9c15f26237ba581fc323
patrycjalewandowska <24plewandowska@gmail.com>
2022-08-14 15:48:55 +0200

    add mvvm in mainviewmodel

---
+++ AppArduino/AppArduino/ViewModel/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.ComponentModel;
+
+namespace AppArduino.ViewModel;
+
+public partial class MainViewModel : ObservableObject
+{
+    [ObservableProperty]
+    string text;
+    
+}
+
